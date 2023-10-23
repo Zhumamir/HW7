584b7ff2d0dd7cb4c0d9056a1105407631781c96
Amir Zhumagaliev <leasholea@gmail.com>
2023-10-23 23:31:48 +0600

    Добавьте файлы проекта.

---
+++ Day7(Tanks)/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyClassLib.WorldOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Tanks_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank t34 = new Tank("Т-34");
+            Tank pantera = new Tank("Pantera");
+
+            Console.WriteLine(t34.GetTankParameters());
+            Console.WriteLine(pantera.GetTankParameters());
+
+            if (t34 ^ pantera)
+            {
+                Console.WriteLine("Т-34 победил в бою!");
+            }
+            else
+            {
+                Console.WriteLine("Pantera победил в бою!");
+            }
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammunitionLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+
+        public Tank(string name)
+        {
+            tankName = name;
+            Random random = new Random();
+            ammunitionLevel = random.Next(0, 101);
+            armorLevel = random.Next(0, 101);
+            maneuverabilityLevel = random.Next(0, 101);
+        }
+
+        public string GetTankParameters()
+        {
+            return $"Танк: {tankName}, Боезапас: {ammunitionLevel}%, Броня: {armorLevel}%, Маневренность: {maneuverabilityLevel}%";
+        }
+
+        public static bool operator ^(Tank t34, Tank pantera)
+        {
+            int winCriteria = 0;
+
+            if (t34.ammunitionLevel > pantera.ammunitionLevel)
+                winCriteria++;
+            if (t34.armorLevel > pantera.armorLevel)
+                winCriteria++;
+            if (t34.maneuverabilityLevel > pantera.maneuverabilityLevel)
+                winCriteria++;
+
+            return winCriteria >= 2;
+        }
+    }
+}
